df20174bf5a689f0bff90f152219021c864c54db
Lukas Weilguny <lukas.weilguny@24speed.at>
2018-11-07 14:54:01 +0100

    update setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
     
 setup(
     name='deviaTE',
-    version='0.2',
+    version='0.2.1',
     description='Analysis and visualization of mobile genetic element composition',
     long_description=readme(),
     license='LICENSE',
