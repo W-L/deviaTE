d221693c6871ff7c40c8b454ee679841150d9faa
Lukas Weilguny <lukas.weilguny@24speed.at>
2019-02-22 18:52:11 +0100

    increment version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='deviaTE',
-    version='0.3.1',
+    version='0.3.2',
     description='Analysis and visualization of mobile genetic element composition',
     long_description=readme(),
     license='LICENSE',
