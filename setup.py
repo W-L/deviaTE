9991d8cb90379de0bf53bef63087c02042d21fb1
Lukas Weilguny <lukas.weilguny@24speed.at>
2019-02-22 11:42:33 +0100

    increase version num

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 def readme():
     with open('README.md') as f:
         return f.read()
-    
-    
+
+
 setup(
     name='deviaTE',
-    version='0.2.1.1',
+    version='0.3',
     description='Analysis and visualization of mobile genetic element composition',
     long_description=readme(),
     license='LICENSE',
@@ -18,10 +18,10 @@ setup(
     include_package_data=True,
     install_requires=['pysam', 'pandas'],
     scripts=['bin/deviaTE_analyse',
-    		 'bin/deviaTE_plot',
-    		 'bin/deviaTE_prep',
+             'bin/deviaTE_plot',
+             'bin/deviaTE_prep',
              'bin/deviaTE_fuse',
-    		 'bin/deviaTE',
+             'bin/deviaTE',
              'bin/deviaTE_trim.pl',
              'bin/deviaTE_trim.pm']
 )
