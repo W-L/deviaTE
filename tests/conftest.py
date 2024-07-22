a18a0281522451bd12bddb635344480777f3c5c7
W-L <weilguny.lukas@gmail.com>
2024-07-22 14:13:53 +0200

    add nanopore tests, relocate fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 from pathlib import Path
+from types import SimpleNamespace
 
 import pytest
 
 from deviaTE.paf import Paf
 from deviaTE.analyse import CoverageConverter
 from deviaTE.mapping import Mapper
-
+from deviaTE.config import Config
+from deviaTE.reference import InputFile
 
 
 @pytest.fixture(scope="module")
@@ -16,6 +18,16 @@ def testfq():
 @pytest.fixture(scope="module")
 def testfq_gz():
     return "../data/jockey_dmel.fastq.gz"
+
+
+@pytest.fixture(scope="module")
+def testfq_nanopore_short():
+    return "../data/SRR28208594_1_FBte0000400_270.fq"
+
+
+@pytest.fixture(scope="module")
+def testfq_nanopore_long():
+    return "../data/SRR28208594_1_FBte0001206_285.fq"
 
 
 @pytest.fixture(scope="module")
@@ -39,8 +51,21 @@ def testfam():
 
 
 @pytest.fixture(scope="module")
+def testfam_short():
+    return "FBte0000400"
+
+
+@pytest.fixture(scope="module")
+def testfam_long():
+    return "FBte0001206"
+
+
+@pytest.fixture(scope="module")
 def testresults():
     return "../data/jockey_dmel.fastq.FBte0000088.deviate"
+
+
+##################################################################################
 
 
 @pytest.fixture(scope="module")
@@ -66,5 +91,172 @@ def increments(paf_dict):
     cc = CoverageConverter()
     incr = cc.convert_records(paf_dict=paf_dict)
     return incr
+
+
+##################################################################################
+
+@pytest.fixture(scope="module")
+def args_short_scg(testfq, testfam):
+    return SimpleNamespace(
+        input=testfq,
+        families=[testfam],
+        single_copy_genes=['Dmel_rpl32', 'Dmel_Act5C', 'Dmel_RpII140', 'Dmel_piwi', 'Dmel_p53']
+    )
+
+
+@pytest.fixture(scope="module")
+def args_short_rpm(testfq, testfam):
+    return SimpleNamespace(
+        input=testfq,
+        families=[testfam],
+        rpm=True
+    )
+
+
+@pytest.fixture(scope="module")
+def args_short(testfq, testfam):
+    return SimpleNamespace(
+        input=testfq,
+        families=[testfam],
+    )
+
+
+@pytest.fixture(scope="module")
+def args_short_gz(testfq_gz, testfam):
+    return SimpleNamespace(
+        input=testfq_gz,
+        families=[testfam],
+    )
+
+
+@pytest.fixture(scope="module")
+def args_short_tar(testfq, testfam):
+    return SimpleNamespace(
+        input=testfq,
+        families=[testfam],
+        tar=True
+    )
+
+
+@pytest.fixture(scope="module")
+def args_nanopore_short(testfq_nanopore_short, testfam_short):
+    return SimpleNamespace(
+        input=testfq_nanopore_short,
+        families=[testfam_short],
+        preset="map-ont"
+    )
+
+
+@pytest.fixture(scope="module")
+def args_nanopore_long(testfq_nanopore_long, testfam_long):
+    return SimpleNamespace(
+        input=testfq_nanopore_long,
+        families=[testfam_long],
+        preset="map-ont"
+    )
+
+
+##################################################################################
+
+
+@pytest.fixture(scope="module")
+def scg_conf(args_short_scg):
+    conf = Config(args_debug=args_short_scg)
+    return conf
+
+
+@pytest.fixture(scope="module")
+def rpm_conf(args_short_rpm):
+    conf = Config(args_debug=args_short_rpm)
+    return conf
+
+
+@pytest.fixture(scope="module")
+def short_conf(args_short):
+    conf = Config(args_debug=args_short)
+    return conf
+
+
+@pytest.fixture(scope="module")
+def short_conf_tar(args_short_tar):
+    conf = Config(args_debug=args_short_tar)
+    return conf
+
+
+@pytest.fixture(scope="module")
+def short_conf_gz(args_short_gz):
+    conf = Config(args_debug=args_short_gz)
+    return conf
+
+
+@pytest.fixture(scope="module")
+def nanopore_conf_short(args_nanopore_short):
+    conf = Config(args_debug=args_nanopore_short)
+    return conf
+
+
+@pytest.fixture(scope="module")
+def nanopore_conf_long(args_nanopore_long):
+    conf = Config(args_debug=args_nanopore_long)
+    return conf
+
+
+##################################################################################
+
+
+@pytest.fixture(scope="module")
+def infile_converted_scg(scg_conf, testfq):
+    infile = InputFile(conf=scg_conf, infile=Path(testfq))
+    infile.analyse_coverage()
+    return infile
+
+
+@pytest.fixture(scope="module")
+def infile_converted_rpm(rpm_conf, testfq):
+    infile = InputFile(conf=rpm_conf, infile=Path(testfq))
+    infile.analyse_coverage()
+    return infile
+
+
+@pytest.fixture(scope="module")
+def infile_converted_rpm_gz(rpm_conf, testfq_gz):
+    infile = InputFile(conf=rpm_conf, infile=Path(testfq_gz))
+    infile.analyse_coverage()
+    return infile
+
+
+@pytest.fixture(scope="module")
+def infile_converted(short_conf, testfq):
+    infile = InputFile(conf=short_conf, infile=Path(testfq))
+    infile.analyse_coverage(force_map=True)
+    return infile
+
+
+@pytest.fixture(scope="module")
+def infile_converted_gz(short_conf_gz, testfq_gz):
+    infile = InputFile(conf=short_conf_gz, infile=Path(testfq_gz))
+    infile.analyse_coverage()
+    return infile
+
+
+@pytest.fixture(scope="module")
+def infile_converted_tar(short_conf_tar, testfq_gz):
+    infile = InputFile(conf=short_conf_tar, infile=Path(testfq_gz))
+    infile.analyse_coverage()
+    return infile
+
+
+@pytest.fixture(scope="module")
+def infile_converted_nanopore_short(nanopore_conf_short, testfq_nanopore_short):
+    infile = InputFile(conf=nanopore_conf_short, infile=Path(testfq_nanopore_short))
+    infile.analyse_coverage(force_map=True)
+    return infile
+
+
+@pytest.fixture(scope="module")
+def infile_converted_nanopore_long(nanopore_conf_long, testfq_nanopore_long):
+    infile = InputFile(conf=nanopore_conf_long, infile=Path(testfq_nanopore_long))
+    infile.analyse_coverage(force_map=True)
+    return infile
 
 
--- tests/test_reference.py
+++ tests/test_reference.py
@@ -1,127 +1,16 @@
-from types import SimpleNamespace
 from pathlib import Path
 import sys
 import tarfile
 
 import numpy as np
-import pytest
 
 import deviaTE.reference
 import deviaTE.config
 import deviaTE.mapping
 
 
-testfq = "../data/jockey_dmel.fastq"
-testfq_gz = "../data/jockey_dmel.fastq.gz"
-testpaf = "../data/jockey_dmel.fastq.paf"
-testfam = "FBte0000088"
 
-
-args_short_scg = SimpleNamespace(
-    input=testfq,
-    families=[testfam],
-    single_copy_genes=['Dmel_rpl32', 'Dmel_Act5C', 'Dmel_RpII140', 'Dmel_piwi', 'Dmel_p53']
-)
-
-
-args_short_rpm = SimpleNamespace(
-    input=testfq,
-    families=[testfam],
-    rpm=True
-)
-
-args_short = SimpleNamespace(
-    input=testfq,
-    families=[testfam],
-)
-
-
-args_short_gz = SimpleNamespace(
-    input=testfq_gz,
-    families=[testfam],
-)
-
-
-args_short_tar = SimpleNamespace(
-    input=testfq,
-    families=[testfam],
-    tar=True
-)
-
-
-@pytest.fixture
-def scg_conf():
-    conf = deviaTE.config.Config(args_debug=args_short_scg)
-    return conf
-
-
-@pytest.fixture
-def rpm_conf():
-    conf = deviaTE.config.Config(args_debug=args_short_rpm)
-    return conf
-
-
-@pytest.fixture
-def short_conf():
-    conf = deviaTE.config.Config(args_debug=args_short)
-    return conf
-
-
-@pytest.fixture
-def short_conf_tar():
-    conf = deviaTE.config.Config(args_debug=args_short_tar)
-    return conf
-
-
-@pytest.fixture
-def short_conf_gz():
-    conf = deviaTE.config.Config(args_debug=args_short_gz)
-    return conf
-
-
-@pytest.fixture
-def infile_converted_scg(scg_conf):
-    infile = deviaTE.reference.InputFile(conf=scg_conf, infile=Path(testfq))
-    infile.analyse_coverage()
-    return infile
-
-
-@pytest.fixture
-def infile_converted_rpm(rpm_conf):
-    infile = deviaTE.reference.InputFile(conf=rpm_conf, infile=Path(testfq))
-    infile.analyse_coverage()
-    return infile
-
-
-@pytest.fixture
-def infile_converted_rpm_gz(rpm_conf):
-    infile = deviaTE.reference.InputFile(conf=rpm_conf, infile=Path(testfq_gz))
-    infile.analyse_coverage()
-    return infile
-
-
-@pytest.fixture
-def infile_converted(short_conf):
-    infile = deviaTE.reference.InputFile(conf=short_conf, infile=Path(testfq))
-    infile.analyse_coverage(force_map=True)
-    return infile
-
-
-@pytest.fixture
-def infile_converted_gz(short_conf_gz):
-    infile = deviaTE.reference.InputFile(conf=short_conf_gz, infile=Path(testfq_gz))
-    infile.analyse_coverage()
-    return infile
-
-
-@pytest.fixture
-def infile_converted_tar(short_conf_tar):
-    infile = deviaTE.reference.InputFile(conf=short_conf_tar, infile=Path(testfq_gz))
-    infile.analyse_coverage()
-    return infile
-
-
-def test_scg_normfac(scg_conf, increments):
+def test_scg_normfac(scg_conf, increments, args_short_scg):
     sequence_lib = scg_conf.sequences
     scg = deviaTE.reference.single_gene_norm_fac(
         scgs=args_short_scg.single_copy_genes,
@@ -131,7 +20,7 @@ def test_scg_normfac(scg_conf, increments):
 
 
 
-def test_inputfile_analyse_coverage(infile_converted_scg):
+def test_inputfile_analyse_coverage(infile_converted_scg, testfam):
     infile = infile_converted_scg
     assert isinstance(infile.mapper, deviaTE.mapping.Mapper)
     assert len(infile.incr[testfam]) == 4747
@@ -139,18 +28,19 @@ def test_inputfile_analyse_coverage(infile_converted_scg):
     assert np.allclose(infile.scg_norm_fac, 0.0059049079754601224)
 
 
-def test_inputfile_analyse_coverage_gz(infile_converted_gz):
+def test_inputfile_analyse_coverage_gz(infile_converted_gz, testfam):
     infile = infile_converted_gz
     assert isinstance(infile.mapper, deviaTE.mapping.Mapper)
     assert len(infile.incr[testfam]) == 4747
     assert sys.getsizeof(infile.incr[testfam]) == 41880
 
 
-def test_fams_scg(infile_converted_scg):
+def test_fams_scg(infile_converted_scg, testfam):
     infile_converted_scg.analyse_families()
     assert len(infile_converted_scg.results_files) == 1
-    assert Path(f"jockey_dmel.fastq.{testfam}.deviate").is_file()
-    with open(f"jockey_dmel.fastq.{testfam}.deviate", 'r') as outf:
+    result = f"jockey_dmel.fastq.{testfam}.deviate"
+    assert Path(result).is_file()
+    with open(result, 'r') as outf:
         lines = outf.readlines(1000)
     ll = lines[5]
     test_num = float(ll.split(' ')[4])
@@ -160,31 +50,36 @@ def test_fams_scg(infile_converted_scg):
     ins, inshq = float(jj.split(' ')[2]), float(jj.split(' ')[4])
     assert np.allclose(ins, 33773.88337558856)
     assert np.allclose(inshq, 29360.645935737568)
+    Path(result).unlink()
 
 
-def test_fams_rpm(infile_converted_rpm):
+def test_fams_rpm(infile_converted_rpm, testfam):
     infile_converted_rpm.analyse_families()
     assert len(infile_converted_rpm.results_files) == 1
-    assert Path(f"jockey_dmel.fastq.{testfam}.deviate").is_file()
-    with open(f"jockey_dmel.fastq.{testfam}.deviate", 'r') as outf:
+    result = f"jockey_dmel.fastq.{testfam}.deviate"
+    assert Path(result).is_file()
+    with open(result, 'r') as outf:
         lines = outf.readlines(1000)
     ll = lines[5]
     test_num = float(ll.split(' ')[4])
     assert np.allclose(test_num, 207.68431983385256)
+    Path(result).unlink()
 
 
-def test_fams_rpm_gz(infile_converted_rpm_gz):
+def test_fams_rpm_gz(infile_converted_rpm_gz, testfam):
     infile_converted_rpm_gz.analyse_families()
     assert len(infile_converted_rpm_gz.results_files) == 1
-    assert Path(f"jockey_dmel.fastq.gz.{testfam}.deviate").is_file()
-    with open(f"jockey_dmel.fastq.gz.{testfam}.deviate", 'r') as outf:
+    result = f"jockey_dmel.fastq.gz.{testfam}.deviate"
+    assert Path(result).is_file()
+    with open(result, 'r') as outf:
         lines = outf.readlines(1000)
     ll = lines[5]
     test_num = float(ll.split(' ')[4])
     assert np.allclose(test_num, 207.68431983385256)
+    Path(result).unlink()
 
 
-def test_fams_tar(infile_converted_tar):
+def test_fams_tar(infile_converted_tar, testfam):
     infile_converted_tar.analyse_families()
     assert len(infile_converted_tar.results_files) == 1
     tar = Path("jockey_dmel.fastq.gz.deviate.tar")
@@ -194,21 +89,54 @@ def test_fams_tar(infile_converted_tar):
         assert len(members) == 1
         assert members[0].name == f'jockey_dmel.fastq.gz.{testfam}.deviate'
         assert members[0].size == 330285
+    tar.unlink()
 
 
-def test_viz(infile_converted):
+
+def test_fams_nanopore_short(infile_converted_nanopore_short, testfam_short):
+    infile_converted_nanopore_short.analyse_families()
+    assert len(infile_converted_nanopore_short.results_files) == 1
+    result_file = f"SRR28208594_1_FBte0000400_270.fq.{testfam_short}.deviate"
+    assert Path(result_file).is_file()
+    with open(result_file, 'r') as outf:
+        lines = outf.readlines(1000)
+    ll = lines[5]
+    test_num = float(ll.split(' ')[4])
+    assert np.allclose(test_num, 85)
+    Path(result_file).unlink()
+
+
+def test_fams_nanopore_long(infile_converted_nanopore_long, testfam_long):
+    infile_converted_nanopore_long.analyse_families()
+    assert len(infile_converted_nanopore_long.results_files) == 1
+    result_file = f"SRR28208594_1_FBte0001206_285.fq.{testfam_long}.deviate"
+    assert Path(result_file).is_file()
+    with open(result_file, 'r') as outf:
+        lines = outf.readlines(1000)
+    ll = lines[5]
+    test_num = float(ll.split(' ')[4])
+    print(test_num)
+    assert np.allclose(test_num, 0)
+    Path(result_file).unlink()
+
+
+def test_viz(infile_converted, testfam):
     infile_converted.analyse_families()
     infile_converted.visualise()
-    assert Path(f"jockey_dmel.fastq.{testfam}.deviate.pdf").is_file()
+    res = f"jockey_dmel.fastq.{testfam}.deviate.pdf"
+    assert Path(res).is_file()
+    Path(res).unlink()
 
 
-def test_viz_gz(infile_converted_gz):
+def test_viz_gz(infile_converted_gz, testfam):
     infile_converted_gz.analyse_families()
     infile_converted_gz.visualise()
-    assert Path(f"jockey_dmel.fastq.gz.{testfam}.deviate.pdf").is_file()
+    res = f"jockey_dmel.fastq.gz.{testfam}.deviate.pdf"
+    assert Path(res).is_file()
+    Path(res).unlink()
 
 
-def test_viz_tar(infile_converted_tar):
+def test_viz_tar(infile_converted_tar, testfam):
     infile_converted_tar.analyse_families()
     infile_converted_tar.visualise()
     assert not Path(f"jockey_dmel.fastq.gz.{testfam}.deviate.pdf").is_file()
@@ -218,9 +146,16 @@ def test_viz_tar(infile_converted_tar):
         members = tarf.getmembers()
         assert len(members) == 1
         assert members[0].name == f'jockey_dmel.fastq.gz.{testfam}.deviate.pdf'
-        assert members[0].size == 139940
+        assert members[0].size == 139913
+    tar.unlink()
 
 
+def test_viz_nanopore(infile_converted_nanopore_short, testfam_short):
+    infile_converted_nanopore_short.analyse_families()
+    infile_converted_nanopore_short.visualise()
+    res = f"SRR28208594_1_FBte0000400_270.fq.{testfam_short}.deviate.pdf"
+    assert Path(res).is_file()
+    Path(res).unlink()
 
 
 
